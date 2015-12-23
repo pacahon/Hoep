0d4c0507dcb34d1b63a099c8b08656b66394bc37
pacahon <pacabest@gmail.com>
2015-12-23 13:32:30 +0300

    add --math and --math-explicit support to py3 version

--- setup.py
+++ setup.py
@@ -199,7 +199,7 @@ _sources = [
 
 setup(
     name = 'hoep',
-    version = '2.0.1',
+    version = '2.0.2',
     author = 'Andrew Fricke',
     author_email = 'andrew@uhnomoli.com',
     url = 'https://github.com/Anomareh/Hoep',
